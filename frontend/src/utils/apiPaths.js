e12897cd35ec2b10315929df105e050c608b34a7
Muhammad Saad <studentsaad41@gmail.com>
2025-10-18 06:44:40 +0500

    fix import properly

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,39 +1,39 @@
-export const BASE_URL = import.meta.env.REACT_APP_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_PATHS = {
   AUTH: {
-    REGISTER: "/api/auth/register", // Signup
-    LOGIN: "/api/auth/login",       // Authenticate user & return JWT token
-    GET_PROFILE: "/api/auth/profile", // Get logged-in user details
-    UPDATE_PROFILE: "/api/user/profile", // Update profile details
-    DELETE_RESUME: "/api/user/resume",   // Delete resume details
+    REGISTER: "/auth/register", // Signup
+    LOGIN: "/auth/login",       // Authenticate user & return JWT token
+    GET_PROFILE: "/auth/profile", // Get logged-in user details
+    UPDATE_PROFILE: "/user/profile", // Update profile details
+    DELETE_RESUME: "/user/resume",   // Delete resume details
   },
 
   DASHBOARD: {
-    OVERVIEW: "/api/analytics/overview",
+    OVERVIEW: "/analytics/overview",
   },
 
   JOBS: {
-    GET_ALL_JOBS: "/api/jobs",
-    GET_JOB_BY_ID: (id) => `/api/jobs/${id}`,
-    POST_JOB: "/api/jobs",
-    GET_JOBS_EMPLOYER: "/api/jobs/get-jobs-employer",
-    UPDATE_JOB: (id) => `/api/jobs/${id}`,
-    TOGGLE_CLOSE: (id) => `/api/jobs/${id}/toggle-close`,
-    DELETE_JOB: (id) => `/api/jobs/${id}`,
-    SAVE_JOB: (id) => `/api/save-jobs/${id}`,
-    UNSAVE_JOB: (id) => `/api/save-jobs/${id}`,
-    GET_SAVED_JOBS: "/api/save-jobs/my",
+    GET_ALL_JOBS: "/jobs",
+    GET_JOB_BY_ID: (id) => `/jobs/${id}`,
+    POST_JOB: "/jobs",
+    GET_JOBS_EMPLOYER: "/jobs/get-jobs-employer",
+    UPDATE_JOB: (id) => `/jobs/${id}`,
+    TOGGLE_CLOSE: (id) => `/jobs/${id}/toggle-close`,
+    DELETE_JOB: (id) => `/jobs/${id}`,
+    SAVE_JOB: (id) => `/save-jobs/${id}`,
+    UNSAVE_JOB: (id) => `/save-jobs/${id}`,
+    GET_SAVED_JOBS: "/save-jobs/my",
   },
 
   APPLICATIONS: {
-  APPLY_TO_JOB: (id) => `/api/applications/${id}`,
-  GET_ALL_APPLICATIONS: (id) => `/api/applications/job/${id}`,
-  UPDATE_STATUS: (id) => `/api/applications/${id}/status`,
+  APPLY_TO_JOB: (id) => `/applications/${id}`,
+  GET_ALL_APPLICATIONS: (id) => `/applications/job/${id}`,
+  UPDATE_STATUS: (id) => `/applications/${id}/status`,
 },
 
 IMAGE: {
-  UPLOAD_IMAGE: "/api/auth/upload-image", // Upload profile picture
+  UPLOAD_IMAGE: "/auth/upload-image", // Upload profile picture
 },
 
 };
